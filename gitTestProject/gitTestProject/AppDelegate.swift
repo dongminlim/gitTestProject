d09a30ca78f1d50f797852d701470f66f1ec938e
디엠(임동민) <>
2020-03-26 10:03:51 +0900

    알림기능 커밋

--- gitTestProject/gitTestProject/AppDelegate.swift
+++ gitTestProject/gitTestProject/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //알림기능 구현 완료
+        
+        
         return true
     }
 
